977d5227e6c09e8d5afd86ec47148a8059dd0b52
ashokmachineni <ashok5679m@gmail.com>
2016-08-25 09:37:53 -0400

    Initial commit

---
+++ app/src/main/java/com/ashok/voll/GetDataAdapter.java
@@ -0,0 +1,32 @@
+package com.ashok.voll;
+
+/**
+ * Created by ashok on 8/24/16.
+ */
+public class GetDataAdapter {
+
+    /**
+     * id : 1
+     * image_title : Android 1.5 Cupcake
+     * image_url : http://androidblog.esy.es/images/cupcake-1.png
+     */
+    public String ImageServerUrl;
+    public String ImageTitleName;
+
+    public String getImageServerUrl() {
+        return ImageServerUrl;
+    }
+
+    public void setImageServerUrl(String imageServerUrl) {
+        this.ImageServerUrl = imageServerUrl;
+    }
+
+    public String getImageTitleName() {
+        return ImageTitleName;
+    }
+
+    public void setImageTitleNamee(String Imagetitlename) {
+        this.ImageTitleName = Imagetitlename;
+    }
+
+}
